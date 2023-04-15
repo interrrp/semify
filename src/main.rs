e7f3938fc34c93b3790d3634918636a27e109e8f
watchYourCache <watchYourCache@gmail.com>
2023-04-15 20:57:31 +0800

    Initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args();
+    if args.len() < 2 {
+        println!("Usage: semify <filename>");
+        return;
+    }
+
+    let filename = args.skip(1).next().unwrap();
+    let contents = fs::read_to_string(filename).expect("Could not read file");
+
+    for line in contents.lines() {
+        println!("{}", add_semicolon(line));
+    }
+}
+
+/// Add a semicolon to the end of the line if it should.
+fn add_semicolon(line: &str) -> String {
+    if should_add_semicolon(line) {
+        format!("{};", line)
+    } else {
+        line.to_string()
+    }
+}
+
+/// Determine whether a semicolon should be added to the end of the line.
+fn should_add_semicolon(line: &str) -> bool {
+    !is_str_whitespace(line) && !line.ends_with(":") && !line.ends_with(";")
+}
+
+/// Determine whether a string is empty or contains only whitespace.
+fn is_str_whitespace(s: &str) -> bool {
+    s.chars().all(char::is_whitespace)
 }
